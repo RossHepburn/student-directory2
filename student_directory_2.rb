e79c41da159678c36863ddd1c18db778fcfcdffc
Ross Hepburn <hepburnjrl@gmail.com>
2014-02-27 17:28:42 +0000

    Interactive 1,2,9

--- student_directory_2.rb
+++ student_directory_2.rb
@@ -1,3 +1,26 @@
+def interactive_menu
+ students = []
+ loop do 
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+  selection = gets.chomp
+  case selection
+  when "1"
+    students = input_students
+  when "2"
+    print_header
+    print(students)
+    print_footer(students)
+  when "9"
+    exit
+  else 
+    puts "I don't know what you mean't, try again"
+  end
+ end
+end
+
+
 def print_header
   puts "The students of my cohort at Makers Academy"
   puts "-------------"
@@ -32,7 +55,7 @@ def print_footer(students)
   puts "Overall, we have #{students.length} great students"
 end
 
-students = input_students
+students = interactive_menu
   if !students.empty?
     print_header
     print(students)
