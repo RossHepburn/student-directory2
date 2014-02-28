6c0064eb36823c83dc5766afd9237aca9471e817
Ross Hepburn <hepburnjrl@gmail.com>
2014-02-28 13:33:01 +0000

    Saving added

--- student_directory_2.rb
+++ student_directory_2.rb
@@ -20,6 +20,7 @@ end
 def print_menu    
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save list to students.csv"
   puts "9. Exit" 
 end
 
@@ -35,6 +36,8 @@ def process(selection)
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit
   else 
@@ -47,6 +50,17 @@ def process(selection)
     puts "#{student[:first_name]} #{student[:last_name]} joined Makers Academy in the #{student[:cohort]} cohort".center(100)
   end
 end
+
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:first_name], student[:last_name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
 
 def input_students
 # get user first name
